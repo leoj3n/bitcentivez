fa649368f2482b3b4ff6094306c37f11cfb25ff9
Joel Kuzmarski <leoj3n@gmail.com>
2018-10-31 03:49:37 -0500

    Workaround for steal-conditional culled imports

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,3 +1,5 @@
+import '~/models/feathers-client-rest#?always-false';
+import '~/models/feathers-client-socketio#?always-false';
 import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
 
 console.log('feathers client ...');
