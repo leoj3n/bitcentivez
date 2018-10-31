0716900ceae6bfd08d55836f20f73c34cc5d785f
Joel Kuzmarski <leoj3n@gmail.com>
2018-10-31 04:00:39 -0500

    Still trying to workaround

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,5 +1,3 @@
-import '~/models/feathers-client-rest#?always-false';
-import '~/models/feathers-client-socketio#?always-false';
 import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
 
 console.log('feathers client ...');
