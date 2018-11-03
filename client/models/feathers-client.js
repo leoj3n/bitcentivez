98a0483d4668a0f1e5a94fea168a937e7da5b25f
Joel Kuzmarski <leoj3n@gmail.com>
2018-11-03 18:17:56 -0500

    Try another method

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,3 +1,5 @@
-import feathersClient from '~/models/feathers-client-#{rest-or-socketio}';
+//import feathersClient from '~/models/feathers-client-rest';
+import feathersClientRest from '~/models/feathers-client-rest#?is-window';
+import feathersClientSocketio from '~/models/feathers-client-socketio#?~is-window';
 
-export default feathersClient;
+export default feathersClientRest ? feathersClientRest : feathersClientSocketio;
