66208280fde27012bea6ddd8344350d0f65cf454
Joel Kuzmarski <leoj3n@gmail.com>
2018-11-03 10:44:07 -0500

    Compromise by only using REST

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,7 +1,3 @@
-//import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
-import feathersClientSocketio from '~/models/feathers-client-socketio#?is-window';
-import feathersClientRest from '~/models/feathers-client-rest#?~is-window';
-
-const feathersClient = feathersClientSocketio ? feathersClientSocketio : feathersClientRest;
+import feathersClient from '~/models/feathers-client-rest';
 
 export default feathersClient;
