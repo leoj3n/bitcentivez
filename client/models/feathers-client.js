a48b4a73be6344e3fc3cd9f9c6ecc372e25c5c8c
Joel Kuzmarski <leoj3n@gmail.com>
2018-11-03 10:35:45 -0500

    Attempt boolean conditional workaround

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,3 +1,7 @@
-import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
+//import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
+import feathersClientSocketio from '~/models/feathers-client-socketio#?is-window';
+import feathersClientRest from '~/models/feathers-client-rest#?~is-window';
+
+const feathersClient = feathersClientSocketio ? feathersClientSocketio : feathersClientRest;
 
 export default feathersClient;
