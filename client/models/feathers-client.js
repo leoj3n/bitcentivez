a0b6e189ad8e7c66e164314564b809da35800289
Joel Kuzmarski <leoj3n@gmail.com>
2018-11-03 18:52:20 -0500

    Update

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,5 +1,5 @@
 //import feathersClient from '~/models/feathers-client-rest';
-import feathersClientRest from '~/models/feathers-client-rest#?is-window';
-import feathersClientSocketio from '~/models/feathers-client-socketio#?~is-window';
+import feathersClientRest from '~/models/feathers-client-rest#?bitcentivez-client/models/is-window';
+import feathersClientSocketio from '~/models/feathers-client-socketio#?~bitcentivez-client/models/is-window';
 
 export default feathersClientRest ? feathersClientRest : feathersClientSocketio;
