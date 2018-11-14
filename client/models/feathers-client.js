19505e3ddfded431e79022edd2ca14fe54620adb
Joel Kuzmarski <leoj3n@gmail.com>
2018-11-13 20:37:08 -0600

    Modify path to rest-or-socketio

--- client/models/feathers-client.js
+++ client/models/feathers-client.js
@@ -1,3 +1,3 @@
-import feathersClient from '~/models/feathers-client-#{./rest-or-socketio}';
+import feathersClient from '~/models/feathers-client-#{~/models/rest-or-socketio}';
 
 export default feathersClient;
